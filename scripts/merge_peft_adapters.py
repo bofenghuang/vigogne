2480d2cad85dc7573ca445b836403220b7228984
bofenghuang <bofenghuang7@gmail.com>
2023-07-12 10:50:34 +0200

    Update lora merge script

--- scripts/merge_peft_adapters.py
+++ scripts/merge_peft_adapters.py
@@ -16,27 +16,23 @@ def main(
     tokenizer = AutoTokenizer.from_pretrained(base_model_name_or_path, padding_side="right", use_fast=False)
     base_model = AutoModelForCausalLM.from_pretrained(
         base_model_name_or_path,
-        #    load_in_8bit=True,
         torch_dtype=torch.float16,
+        device_map={"": "cpu"},
+        #    load_in_8bit=True,
+        trust_remote_code=True,
+        low_cpu_mem_usage=True,
         #    offload_folder=offload_dir,
         #    offload_state_dict=True,
-        low_cpu_mem_usage=True,
-        device_map={"": "cpu"},
     )
 
-    first_weight = base_model.model.layers[0].self_attn.q_proj.weight
-    first_weight_old = first_weight.clone()
+    # first_weight = base_model.model.layers[0].self_attn.q_proj.weight
+    # first_weight_old = first_weight.clone()
 
-    lora_model = PeftModel.from_pretrained(
-        base_model,
-        lora_model_name_or_path,
-        torch_dtype=torch.float16,
-        device_map={"": "cpu"},
-    )
-    assert torch.allclose(first_weight_old, first_weight)
+    lora_model = PeftModel.from_pretrained(base_model, lora_model_name_or_path)
+    # assert torch.allclose(first_weight_old, first_weight)
 
     base_model = lora_model.merge_and_unload()
-    assert not torch.allclose(first_weight_old, first_weight)
+    # assert not torch.allclose(first_weight_old, first_weight)
 
     base_model.save_pretrained(
         output_dir,
