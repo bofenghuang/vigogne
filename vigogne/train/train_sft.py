2ad9c7cf1bbae93c188c3ec173623a50deb2dc8c
bofenghuang <bofenghuang7@gmail.com>
2023-07-12 10:51:17 +0200

    Add checkpoint resuming to training script

--- vigogne/train/train_sft.py
+++ vigogne/train/train_sft.py
@@ -26,6 +26,7 @@ from transformers import (
     Trainer,
     TrainingArguments,
 )
+from transformers.trainer_utils import get_last_checkpoint
 
 from vigogne.constants import (
     CHAT,
@@ -259,6 +260,24 @@ def train():
     logger.info(f"Data parameters {data_args}")
     logger.info(f"Training/evaluation parameters {training_args}")
 
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train and not training_args.overwrite_output_dir:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and training_args.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+    
+    # Set seed before initializing model.
+    # set_seed(training_args.seed)
+
     # BitsAndBytesConfig support
     quantization_config = None
     if model_args.load_in_8bit:
@@ -452,7 +471,14 @@ def train():
 
     # Training
     if training_args.do_train:
-        train_result = trainer.train()
+        checkpoint = None
+        if training_args.resume_from_checkpoint is not None:
+            checkpoint = training_args.resume_from_checkpoint
+        elif last_checkpoint is not None:
+            checkpoint = last_checkpoint
+
+        # train_result = trainer.train()
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
 
         # Saves the tokenizer too for easy upload
         # trainer.save_model()
