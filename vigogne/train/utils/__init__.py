f82f7519328667e3070a713424597fefdc99e358
bofenghuang <bofenghuang7@gmail.com>
2023-07-31 01:17:09 +0200

    Add examples packing module

--- vigogne/train/utils/__init__.py
+++ vigogne/train/utils/__init__.py
@@ -4,7 +4,7 @@
 from .callback import LoadBestPeftModelCallback, SavePeftModelCallback
 from .collator import DataCollatorForSupervisedDataset, Seq2SeqDataCollatorForSupervisedDataset
 from .constants import DEFAULT_BOS_TOKEN, DEFAULT_EOS_TOKEN, DEFAULT_PAD_TOKEN, DEFAULT_UNK_TOKEN, IGNORE_INDEX
-from .data import Concatenator
+from .data import Concatenator, ModerateConcatenator
 from .logging import set_verbosity
 from .peft import print_trainable_parameters
 from .process_data import SUPPORTED_PROCESSOR_TEMPLATES
